d925969609cfa796f02ba87b65949d4ebe27df54
lightyears <lightyears1998@hotmail.com>
2019-06-07 16:17:33 +0800

    添加处理密码的逻辑

--- fake.go
+++ fake.go
@@ -5,15 +5,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Human struct {
+	Username string `form:"name"`
+	Password string `form:"pass"`
+}
+
 func main() {
 	r := gin.Default()
 
 	r.Use(cors.Default())
 
 	r.GET("/login", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"token": "db4de92f933092b1f1f6665a742b7192a5eced77",
-		})
+		var human Human
+
+		if (c.ShouldBind(&human)) == nil && human.Password == "dangoyears" {
+			c.JSON(200, gin.H{
+				"token": "db4de92f933092b1f1f6665a742b7192a5eced77",
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"token": "",
+			})
+		}
+
 	})
 
 	r.Run("localhost:12323")

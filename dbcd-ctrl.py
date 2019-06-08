e17126c35609105b889cc4ac6d49cd87d024fcf6
lightyears <lightyears1998@hotmail.com>
2019-06-08 23:11:03 +0800

    尝试添加systemd服务

--- dbcd-ctrl.py
+++ dbcd-ctrl.py
@@ -3,61 +3,63 @@
 from sys import argv
 from argparse import ArgumentParser
 from subprocess import call
-from subprocess import run
-
-
-def update():
-    call(['git', 'pull'])
-
-
-def start():
-    run(['screen', 'go', 'run', 'dbcd.go'])
-
-
-def kill():
-    call(['killall', 'dbcd'])
-
-
-def restart():
-    kill()
-    start()
+from subprocess import Popen
 
 
 def main():
+    # Init argument parser
     parser = ArgumentParser(description='The script controls the status of D̲atab̲ase C̲ourse D̲esign server.')
-    
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-u', '--update', action='store_true', help='update code from git repo')
     group.add_argument('-s', '--start', action='store_true', help='start the server')
     group.add_argument('-k', '--kill', '--stop', action='store_true', help='kill the server')
     group.add_argument('-r', '--restart', action='store_true', help='restart the server')
     
-    # Provide help if no argument is specified.
+    # Print help if no specific argument.
     if len(argv) < 2:
         parser.print_help()
         return
 
-    # Error when more than one argument is passed.
+    # Error when passing two or more arguments.
     if len(argv) > 2:
         parser.error('passing more than one argument is not allowed')
-        
+    
+    # Parse argument.
     args = parser.parse_args()
 
+    # Act according to parsed argument.
     if args.update:
-        update()
+        update_server()
         return
 
     if args.start:
-        start()
+        start_server()
         return
 
     if args.kill:
-        kill()
+        kill_server()
         return
 
     if args.restart:
-        restart()
+        restart_server()
         return
+
+
+def update_server():
+    call(['git', 'pull'])
+
+
+def start_server():
+    Popen(['go', 'run', 'dbcd.go'])
+
+
+def kill_server():
+    call(['killall', 'dbcd'])
+
+
+def restart_server():
+    kill_server()
+    start_server()
 
 
 if __name__ == "__main__":

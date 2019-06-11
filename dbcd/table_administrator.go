f030c3ee13bfef83ab3e718b5cd30940814c6829
lightyears <lightyears1998@hotmail.com>
2019-06-11 20:23:00 +0800

    添加Administrator表的抽象

---
+++ dbcd/table_administrator.go
@@ -0,0 +1,16 @@
+package dbcd
+
+// Administrator 表“Administrator”的抽象
+type Administrator struct {
+	LoginName string
+	PassHash  string
+}
+
+// GetAdministratorByLoginName 返回具有LoginName的Administrator
+func (engine *Engine) GetAdministratorByLoginName(name string) Administrator {
+	var admin Administrator
+	query := `select "AdminLoginName", "AdminPassHash" from "Administrator" where "AdminLoginName"=:1`
+	row := engine.db.QueryRow(query, name)
+	row.Scan(&admin.LoginName, &admin.PassHash)
+	return admin
+}

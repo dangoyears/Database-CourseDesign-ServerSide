32b1724d2fe61e79bdbb2097ffe5cb3d9238fccf
lightyears <lightyears1998@hotmail.com>
2019-06-13 15:02:02 +0800

    添加测试数据

---
+++ scripts/test_data.sql
@@ -0,0 +1,11 @@
+insert into "Human" ("Name") 
+values ('仁占位');
+
+insert into "College" ("CollegeName")
+values ('计算机科学与网络工程学院');
+
+select * from "College";
+
+select * from "Class";
+
+select * from "Human";

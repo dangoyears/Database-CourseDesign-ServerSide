c2feb6c9a6bc622992e0694fe83c9c1bb5a2d330
lightyears1998 <lightyears1998@hotmail.com>
2019-06-13 22:44:03 +0800

    Academic映射

--- dbcd/table_academic_year.go
+++ dbcd/table_academic_year.go
@@ -1,29 +1,37 @@
 package dbcd
 
 import (
-	"database/sql"
 	"log"
 )
 
-// AcademicYear 是表“AcademicYear”的模型
+// AcademicYear 表“AcademicYear”的模型
 type AcademicYear struct {
 	AcademicYear int
 }
 
-// TestInsertIntoAcademicYear 测试能否向表中插入数据
-func TestInsertIntoAcademicYear(db *sql.DB) {
-	var (
-		err      error
-		testYear = 3030
-	)
+// CreateAcademicYear 创建年份为year的学年。
+func (engine *Engine) CreateAcademicYear(year int) {
+	query := `insert into "AcademicYear" values (:1)`
+	_, err := engine.db.Exec(query, year)
+	if err != nil {
+		log.Println(err)
+	}
+}
 
-	_, err = db.Exec(`INSERT INTO "AcademicYear" ("AcademicYear") VALUES (:1)`, testYear)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	_, err = db.Exec(`DELETE FROM "AcademicYear" WHERE "AcademicYear"=:1`, testYear)
+// DeleteAcademicYear 删除年份为year的学年。
+func (engine *Engine) DeleteAcademicYear(yaer int) {
+	query := `delete from "AcademicYear" where "AcademicYear"."AcademicYear"=:1`
+	_, err := engine.db.Exec(query, yaer)
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+// TestAcademicYear 测试能否向表中插入数据
+func (engine *Engine) TestAcademicYear() {
+	const testYear = 3030
+
+	log.Println("Testing table AcademicYear.")
+	engine.CreateAcademicYear(testYear)
+	engine.DeleteAcademicYear(testYear)
 }
